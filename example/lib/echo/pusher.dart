baf0274fd26d6a0677e6f55eff5d4480a3bfdd2e
AbdoPrDZ <abdopr47@gmail.com>
2024-05-13 22:09:02 +0100

    Edit example

--- example/lib/echo/pusher.dart
+++ example/lib/echo/pusher.dart
@@ -4,22 +4,19 @@ import 'package:pusher_client_fixed/pusher_client_fixed.dart';
 const String appId = "1321495";
 const String key = "037c47e0cbdc81fb7144";
 const String cluster = "mt1";
-const String hostAuthEndPoint = "http://192.168.1.100/broadcasting/auth";
-const String token = "1|4X1WDn5stL0e8FK9TDRNodkTcc7CrT6GsKzDf9Cd62b08640";
+const String hostAuthEndPoint = "http://localhost/broadcasting/auth";
+const String token = "--TOKEN--";
 
 Echo<PusherClient, PusherChannel> initPusherClient(
   Function(String message) log,
-) {
-  Echo<PusherClient, PusherChannel> pusherEcho = Echo.pusher(
-    key,
-    authEndPoint: hostAuthEndPoint,
-    authHeaders: {'Authorization': 'Bearer $token'},
-    cluster: cluster,
-    enableLogging: true,
-    autoConnect: false,
-  );
-  pusherEcho.connector.onConnectError(
-    (error) => log('[Pusher Connection Error]: ${error?.message}'),
-  );
-  return pusherEcho;
-}
+) =>
+    Echo.pusher(
+      key,
+      authEndPoint: hostAuthEndPoint,
+      authHeaders: {'Authorization': 'Bearer $token'},
+      cluster: cluster,
+      enableLogging: true,
+      autoConnect: false,
+    )..connector.onConnectError(
+        (error) => log('[Pusher Connection Error]: ${error?.message}'),
+      );
